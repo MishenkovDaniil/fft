150385ed4f5f240edbdd2305d17a0933bb32c1da
MishenkovDaniil <mishenkovdaniil@mail.ru>
2022-11-25 14:04:42 +0300

    add examples of working with number and polynomials

--- main.cpp
+++ main.cpp
@@ -8,14 +8,18 @@
 
 int main ()
 {
-    int p_1[8] = {0xF, 3};
-    int p_2[8] = {1, 0xC};
-    int p_res[8] = {};
+    int num_1[16] = {3, 4, 5, 7, 8, 9, 9, 5};
+    int num_2[16] = {9, 8, 7, 7, 7, 6, 9, 9};
+    int num_res[16] = {};
 
-    mult_polynomials (p_1, p_2, 4, p_res);
-    //mult_nums (10, p_1, p_2, 8, p_res);
-    print_poly_mult_res (p_res, 4);
-    //print_nums_mult_res (10, p_res, 8);
+    mult_nums (10, num_1, num_2, 16, num_res);
+    print_nums_mult_res (10, num_res, 16);
+
+    int p_1[16] = {3, 4, 5, 7, 8, 9, 9, 5};
+    int p_2[16] = {9, 8, 7, 7, 7, 6, 9, 9};
+    int p_res[16] = {};
+    mult_polynomials (p_1, p_2, 16, p_res);
+    print_poly_mult_res (p_res, 16);
 
     return 0;
 }
